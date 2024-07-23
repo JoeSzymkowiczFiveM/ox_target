13288df0f81895475d5e469221d310407eb16f2c
Joe Szymkowiczgit config --global user.email <joe.szymkowicz+githubfivem@gmail.com>
2024-07-23 15:03:15 -0500

    fix: compat with lua core

--- client/framework/ox.lua
+++ client/framework/ox.lua
@@ -1,8 +1,8 @@
-if not lib.checkDependency('ox_core', '0.21.3', true) then return end
+if not lib.checkDependency('ox_core', '0.19.0', true) then return end
 
 local Ox = require '@ox_core.lib.init' --[[@as OxClient]]
 local utils = require 'client.utils'
-local player = Ox.GetPlayer()
+local player = Ox.GetPlayerData()
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function utils.hasPlayerGotGroup(filter)
